3f137f7e3b707aafd73bf1a98205541fc8867a66
cuprehan <51897056+cuprehan@users.noreply.github.com>
2019-11-20 20:57:53 -0500

    Basic outline created

--- Main game.py
+++ Main game.py
@@ -1,2 +1,9 @@
 import arcade
 
+WINDOW_WIDTH = 10000
+WINDOW_HEIGHT = 1000
+GAME_TITLE = "Adventure Game"
+
+class AdventureGame(arcade.Window):
+    def __init__(self):
+        super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, GAME_TITLE)
