9dc019d85a4b3f74ee90b0badeda826b4bf2182d
cuprehan <51897056+cuprehan@users.noreply.github.com>
2019-11-21 11:14:18 -0500

    Main function added. screen opens.

--- Main game.py
+++ Main game.py
@@ -9,3 +9,15 @@ class AdventureGame(arcade.Window):
     def __init__(self):
         super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, GAME_TITLE)
         arcade.set_background_color(arcade.color.PICTON_BLUE)
+
+    def setup(self):
+        pass
+
+
+def main():
+    window = AdventureGame()
+    window.setup()
+    arcade.run()
+
+if __name__ == '__main__':
+    main()
