88d7e708ecfaa14f34f4564fe7b8f9017cd35e4e
cuprehan <51897056+cuprehan@users.noreply.github.com>
2019-11-21 11:43:30 -0500

    added some code to setup function as well as an image. setup function is not yet complete. no noticeable problems so far

--- Main game.py
+++ Main game.py
@@ -4,14 +4,17 @@ WINDOW_WIDTH = 10000
 WINDOW_HEIGHT = 1000
 GAME_TITLE = "Adventure Game"
 
+player_lev1 = arcade.load_texture("manP1.png")
+
 
 class AdventureGame(arcade.Window):
     def __init__(self):
         super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, GAME_TITLE)
+        self.levels_list = None
         arcade.set_background_color(arcade.color.PICTON_BLUE)
 
     def setup(self):
-        pass
+        self.levels_list = arcade.SpriteList
 
 
 def main():
